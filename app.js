4672e5e6d38a9e432ac9026b8183a720b2f160eb
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-08-16 14:31:55 -0300

    linkeando la marca  al home

--- app.js
+++ app.js
@@ -5,9 +5,27 @@ const app = express();
 
 
 app.use(express.static(path.join(__dirname,'./Public')));
+app.use(express.static(path.join(__dirname,'./Views')));
 
 app.listen(3030,() => { console.log('Servidor corriendo, puerto 3030');});
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.sendFile(path.resolve(__dirname,'./views/index.html'))
 } );
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+  } );
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/productCart.html'))
+  } );
+
+app.get('/detalleDeProducto', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))
+  } );
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+  } );
+
