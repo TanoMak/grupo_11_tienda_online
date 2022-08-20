c3dc2be952286faf3d789b712302ecee8db62700
FNogueira2023 <f.nogueira2022@outlook.com>
2022-08-19 23:16:06 -0300

    Iniciado el sidebar

--- Public/js/menu.js
+++ Public/js/menu.js
@@ -1,6 +1,19 @@
+// FIXED NAVBAR
+
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
     nav.classList.toggle('active', window.scrollY > 0)
 
+}) 
+
+// SIDEBAR DEL NAVBAR
+
+const toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+   document.getElementById('sidebar').classList.toggle('active');
 })
+
+console.log()
+
