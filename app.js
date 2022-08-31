d6343feeed2924573d2b154df365b8cebbae84f7
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-08-31 20:25:02 -0300

    corrigiendo nombre de carpetas

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const resolve = require('path');
 const path = require('path');
 const app = express();
 const indexRoutes = require('../grupo_11_tienda_online/routes/index');
 
 
-app.use(express.static(path.join(__dirname,'./Public')));
-//app.use(express.static(path.join(__dirname,'./Views')));
+app.use(express.static(path.join(__dirname,'./public')));
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ const path = require('path');
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../Views/index.html'))
+    res.sendFile(path.resolve(__dirname,'../views/index.html'))
   } );
   
 router.get('/login', (req, res) => {
-      res.sendFile(path.resolve(__dirname,'../Views/login.html'))
+      res.sendFile(path.resolve(__dirname,'../views/login.html'))
     } );
   
 router.get('/carrito', (req, res) => {
-      res.sendFile(path.resolve(__dirname,'../Views/productCart.html'))
+      res.sendFile(path.resolve(__dirname,'../views/productCart.html'))
     } );
   
 router.get('/detalleDeProducto', (req, res) => {
-      res.sendFile(path.resolve(__dirname,'../Views/productDetail.html'))
+      res.sendFile(path.resolve(__dirname,'../views/productDetail.html'))
     } );
   
 router.get('/registro', (req, res) => {
-      res.sendFile(path.resolve(__dirname,'../Views/register.html'))
+      res.sendFile(path.resolve(__dirname,'../views/register.html'))
     } );
 
 
