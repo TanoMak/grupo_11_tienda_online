c2b5c7e2de60ce9346eaaba560388ae93292015a
unknown <tanomak>
2022-10-29 10:46:46 -0300

    merge forzado no me tomó varios cambios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,66 @@
-const {validationResult} = require("express-validator");
+const { validationResult } = require('express-validator');
+const fs = require("fs");
+const path = require("path");
+const bcryptjs = require ("bcryptjs")
 
 
-const userController = {
-    login: (req, res) => {
-        return res.render("users/login")
-    },
-    register: (req, res) => {
-        return res.render("users/register");
-    },
-    processRegister: (req, res) => {
-        const resultValidation = validationResult(req);
+function findAll() {
+    const jsonData = fs.readFileSync(
+      path.join(__dirname, "../data/users.json")
+    );
+    const data = JSON.parse(jsonData);
+    return data;
+  }
+  
+  function writeFile(data) {
+    const dataString = JSON.stringify(data, null, 4);
+    fs.writeFileSync(path.join(__dirname, "../data/users.json"), dataString);
+  }
+
+module.exports = {
+  register: (req, res) =>{
+    return res.render ("users/register")
+  },
+
+  processRegister: (req, res) => {
+		const resultValidation = validationResult(req);
+		
+		if (resultValidation.errors.length > 0) {
+			return res.render("users/register", {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		}else{
+      const data = findAll();
+    
+      const newUser = {
+        id: data.length+1,
+        name: req.body.name,
+        lastName: req.body.lastName,
+        phone:req.body.phone,
+        email: req.body.email,
+        adress: req.body.adress,
+        password: bcryptjs.hashSync(req.body.password, 10 ),
+        imageUser: req.file.filename
+      }
+
+      data.push(newUser);
+    
+      console.log(newUser)
         
-        if(resultValidation.errors.length >0) {
-            return res.render("users/register", {
-                errors: resultValidation.mapped(),
-                oldData: req.body
-            });
-        }
+      writeFile(data);
         
+      res.redirect("/users/login");
     }
-};
+  },
 
-module.exports = userController;
+  login: (req, res) => {
+		return res.render('users/login');
+  },
+
+  profile: (req, res) => {
+		return res.render('users/userProfile');
+  },
+
+
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,56 +1,48 @@
 const express = require("express");
-const {body} = require("express-validator");
-
 const router = express.Router();
-
-const multer = require("multer");
 const path = require("path");
+const multer = require("multer");
+const usersController = require("../controllers/userscontrollers");
 
+const validations = require ("../validations/userRegisterValidation")
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./public/images/profile-image")
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../public/images/users"));
     },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    filename: function (req, file, cb) {
+      const newFilename =
+        file.fieldname + Date.now() + "-" + path.extname(file.originalname);
+      cb(null, newFilename);
     },
+});
+  
+const upload = multer({ 
+  storage: storage, 
+  fileFilter: (req, file, cb)=>{
+      const extensionesAceptadas = ['.jpg', '.png', '.txt'];
+
+      const info = path.extname(file.originalname)
+
+      const result = extensionesAceptadas.includes(info)
+
+    
+      if(!result){
+          req.file = file;
+      }
+      //------------------------------//
+
+      cb(null, result);
+  } 
 })
 
-const uploadFile = multer({ storage })
+// Procesar el registro
+router.post('/registro', upload.single('imageUser'), validations.registerFormValidation, usersController.processRegister);
 
-//controlador
-const userController = require("../controllers/userController");
+// Formulario de login
+router.get('/login', usersController.login);
 
-//middlewares
-const validations = [
-    body("name").notEmpty().withMessage("Tienes que escribir tu nombre"),
-    body("phoneNumber").notEmpty().withMessage("Tienes que escribir tu numero de telefono"),
-    body("email").notEmpty().withMessage("Tienes que escribir tu email").bail().isEmail().withMessage("Tienes que escribir un formato de correo valido"),
-    body("password").notEmpty().withMessage("campo obligatorio"),
-    body("profileImage").custom((value, {req}) => {
-        let file = req.file;
-        let acceptedExtensions = [".png",".jpg",".gif"]
-        let fileExtension = path.extname(file.originalname);
+// Perfil de usuario
+router.get('/micuenta', usersController.profile);
 
-        if (!file) {
-            throw new Error("Tienes que subir una imagen");
-        } 
-
-        if (!acceptedExtensions.includes(fileExtension)) {
-            throw new Error("Tienes que subir una imagen");
-        }
-
-        return true;
-    })
-];
-
-//form login
-router.get("/login", userController.login);
-
-//form registro
-router.get("/register", userController.register);
-
-//proceso registro
-router.post("/register", uploadFile.single("profile-image"), validations, userController.processRegister);
-
-module.exports = router;
+module.exports = router
