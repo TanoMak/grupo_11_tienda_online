afd67178cb60d3c610e73ea6611487aa9b5cb4b0
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-12-16 09:34:51 -0300

    evento blur en todos los campos de login y registro

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,40 +1,74 @@
 window.onload = function () {
+  let form = document.getElementById("conteinerId");
+  form.email.focus();
 
-    let form = document.getElementById("conteinerId");
-    form.email.focus()
+  let email = document.querySelector("#email");
 
-    form.addEventListener("submit", function (e) {
-        let errors = [];
-    
-        let email = document.querySelector("#email");
+  email.addEventListener("blur", (e) => {
+    let emailError = document.querySelector(".emailError");
 
-        let password = document.querySelector("#password");
-    
-        if (email.value == "") {
-          errors.push("email obligatorio");
-          email.classList.add("is-invalid");
-          email.classList.remove("is-valid");
-        } else {
-          email.classList.add("is-valid");
-          email.classList.remove("is-invalid");
-        }
-
-        if (password.value == "") { 
-          errors.push("contraseña obligatoria");
-          password.classList.add("is-invalid");
-          password.classList.remove("is-valid");
-        } else {
-          password.classList.add("is-valid");
-          password.classList.remove("is-invalid");
-        }
-    
-        if (errors.length > 0) {
-          e.preventDefault();
-          let ulError = document.querySelector(".errors");
-          ulError.innerHTML = "";
-          for (let i = 0; i < errors.length; i++) {
-            ulError.innerHTML += "<li>" + errors[i] + "<li/>";
-          }
-        }
-      });
+    if (email.value == "") {
+      emailError.innerHTML = "E-mail obligatorio";
+      emailError.classList.add("is-invalid");
+      emailError.classList.remove("is-valid");
+    } else {
+      emailError.classList.add("is-valid");
+      emailError.classList.remove("is-invalid");
+      emailError.innerHTML = "";
     }
+  });
+
+  let password = document.querySelector("#password");
+
+  password.addEventListener("blur", (e) => {
+    let passwordError = document.querySelector(".passwordError");
+
+    if (password.value == "") {
+      passwordError.innerHTML = "Contraseña obligatoria";
+      passwordError.classList.add("is-invalid");
+      passwordError.classList.remove("is-valid");
+    } else {
+      passwordError.classList.add("is-valid");
+      passwordError.classList.remove("is-invalid");
+      passwordError.innerHTML = "";
+    }
+  });
+
+  form.addEventListener("submit", function (e) {
+    let errors = [];
+
+    let email = document.querySelector("#email");
+
+    let password = document.querySelector("#password");
+
+    if (email.value == "") {
+      errors.push("email obligatorio");
+      email.classList.add("is-invalid");
+      email.classList.remove("is-valid");
+    } else {
+      email.classList.add("is-valid");
+      email.classList.remove("is-invalid");
+    }
+
+    if (password.value == "") {
+      errors.push("contraseña obligatoria");
+      password.classList.add("is-invalid");
+      password.classList.remove("is-valid");
+    } else {
+      password.classList.add("is-valid");
+      password.classList.remove("is-invalid");
+    }
+
+    if (errors.length > 0) {
+      e.preventDefault();
+      let ulError = document.querySelector(".errors");
+      ulError.innerHTML = "";
+      for (let i = 0; i < errors.length; i++) {
+        ulError.innerHTML += "<li>" + errors[i] + "<li/>";
+      }
+      ulError.classList.add("is-invalid");
+    } else {
+      alert("Logueado satisfactoriamente");
+    }
+  });
+};
--- public/js/registerValidations.js
+++ public/js/registerValidations.js
@@ -1,7 +1,103 @@
 window.onload = function () {
-
   let form = document.querySelector(".container");
   form.name.focus();
+
+  let name = document.querySelector("#name");
+
+  name.addEventListener("blur", (e) => {
+    let nameError = document.querySelector(".nameError");
+
+    if (name.value == "") {
+      nameError.innerHTML = "Nombre obligatorio";
+      nameError.classList.add("is-invalid");
+      nameError.classList.remove("is-valid");
+    } else {
+      nameError.classList.add("is-valid");
+      nameError.classList.remove("is-invalid");
+      nameError.innerHTML = "";
+    }
+  });
+
+  let lastName = document.querySelector("#lastname");
+
+  lastName.addEventListener("blur", (e) => {
+    let lastNameError = document.querySelector(".lastNameError");
+
+    if (lastName.value == "") {
+      lastNameError.innerHTML = "Apellido obligatorio";
+      lastNameError.classList.add("is-invalid");
+      lastNameError.classList.remove("is-valid");
+    } else {
+      lastNameError.classList.add("is-valid");
+      lastNameError.classList.remove("is-invalid");
+      lastNameError.innerHTML = "";
+    }
+  });
+
+  let phone = document.querySelector("#phone");
+
+  phone.addEventListener("blur", (e) => {
+    let phoneError = document.querySelector(".phoneError");
+
+    if (phone.value == "") {
+      phoneError.innerHTML = "Telefono obligatorio";
+      phoneError.classList.add("is-invalid");
+      phoneError.classList.remove("is-valid");
+    } else {
+      phoneError.classList.add("is-valid");
+      phoneError.classList.remove("is-invalid");
+      phoneError.innerHTML = "";
+    }
+  });
+
+  let email = document.querySelector("#email");
+
+  email.addEventListener("blur", (e) => {
+    let emailError = document.querySelector(".emailError");
+
+    if (email.value == "") {
+      emailError.innerHTML = "E-mail obligatorio";
+      emailError.classList.add("is-invalid");
+      emailError.classList.remove("is-valid");
+    } else {
+      emailError.classList.add("is-valid");
+      emailError.classList.remove("is-invalid");
+      emailError.innerHTML = "";
+    }
+  });
+
+  let password = document.querySelector("#password");
+
+  password.addEventListener("blur", (e) => {
+    let passwordError = document.querySelector(".passwordError");
+
+    if (password.value == "") {
+      passwordError.innerHTML = "Contraseña obligatoria";
+      passwordError.classList.add("is-invalid");
+      passwordError.classList.remove("is-valid");
+    } else {
+      passwordError.classList.add("is-valid");
+      passwordError.classList.remove("is-invalid");
+      passwordError.innerHTML = "";
+    }
+  });
+
+  let address = document.querySelector("#address");
+
+  address.addEventListener("blur", (e) => {
+    let addressError = document.querySelector(".addressError");
+
+    if (address.value == "") {
+      addressError.innerHTML = "Dirección obligatoria";
+      addressError.classList.add("is-invalid");
+      addressError.classList.remove("is-valid");
+    } else {
+      addressError.classList.add("is-valid");
+      addressError.classList.remove("is-invalid");
+      addressError.innerHTML = "";
+    }
+  });
+
 
 
   form.addEventListener("submit", function (e) {
@@ -75,6 +171,9 @@ window.onload = function () {
       for (let i = 0; i < errors.length; i++) {
         ulError.innerHTML += "<li>" + errors[i] + "<li/>";
       }
+      ulError.classList.add("is-invalid");
+    } else {
+      alert("Logueado satisfactoriamente");
     }
   });
 };
