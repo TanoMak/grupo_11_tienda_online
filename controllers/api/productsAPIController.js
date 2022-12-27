cc7dd591faf447c29d422b0821bf75335a9dd9bf
luifer63 <lvix2000@gmail.com>
2022-12-27 16:48:48 -0500

    fix ruta ultimo producto

--- controllers/api/productsAPIController.js
+++ controllers/api/productsAPIController.js
@@ -12,14 +12,15 @@ const Sizes = db.Size
 const Images = db.Image
 
 const productsAPIController = {
-    
+
 
     list: async (req, res) => {
         try {
             let products = await Products.findAll(
-                { include: [{ association: "images"}] 
-              });              
-            
+                {
+                    include: [{ association: "images" }]
+                });
+
             let newData = products.map(product => {
                 return {
                     id: product.id,
@@ -27,66 +28,86 @@ const productsAPIController = {
                     description: product.description,
                     category: product.category_id,
                     image: product.images,
-                    
-                }
-            })  
 
-              let response = {
-                      meta: {
-                      status: 200,
-                      total: products.length,
-                      url: 'api/products/list'
-                  },
-                  data: newData
-              };
-              res.send(response); 
-            
+                }
+            })
+
+            let response = {
+                meta: {
+                    status: 200,
+                    total: products.length,
+                    url: 'api/products/list'
+                },
+                data: newData
+            };
+            res.send(response);
+
         } catch (error) {
-            console.log(error);            
+            console.log(error);
         };
     },
 
     productDetail: async (req, res) => {
-         try {
-            let product = await Products.findByPk(req.params.id,
-                { include: ["colors", "images", "sizes", "category", "line"] 
-              });
-            console.log("El producto ", product);  
-            let response = {
-                  meta: {
-                      status: 200,
-                      total: product.length,
-                      url: 'api/products/detail/:id'
-                  },
-                  data: product
-              };
-              res.send(response);
-            
-        } catch (error) {
-            console.log(error) ;           
-        };
-      },
-
-      prodCategories: async (req, res) => {
         try {
-           let categories = await Categories.findAll();
-           console.log("El producto ", categories);  
-           let response = {
-                 meta: {
-                     status: 200,
-                     total: categories.length,
-                     url: 'api/products/categories'
-                 },
-                 data: categories
-             };
-             res.send(response);
-           
-       } catch (error) {
-           console.log(error) ;           
-       };
-     },
-   
+            let product = await Products.findByPk(req.params.id,
+                {
+                    include: ["colors", "images", "sizes", "category", "line"]
+                });
+            console.log("El producto ", product);
+            let response = {
+                meta: {
+                    status: 200,
+                    total: product.length,
+                    url: 'api/products/detail/:id'
+                },
+                data: product
+            };
+            res.send(response);
+
+        } catch (error) {
+            console.log(error);
+        };
+    },
+
+    prodCategories: async (req, res) => {
+        try {
+            let categories = await Categories.findAll();
+            console.log("El producto ", categories);
+            let response = {
+                meta: {
+                    status: 200,
+                    total: categories.length,
+                    url: 'api/products/categories'
+                },
+                data: categories
+            };
+            res.send(response);
+
+        } catch (error) {
+            console.log(error);
+        };
+    },
+
+    last: async (req, res) => {
+            try {
+                let products = await Products.findOne({ order: [['id', 'DESC']] });
+                console.log("El producto ", products);
+                let response = {
+                    meta: {
+                        status: 200,
+                        total: products.length,
+                        url: 'api/products/categories'
+                    },
+                    data: products
+                };
+                res.send(response);
+
+            } catch (error) {
+                console.log(error);
+            };
+        
+    }   
+            
 
 };
 module.exports = productsAPIController;
- 
